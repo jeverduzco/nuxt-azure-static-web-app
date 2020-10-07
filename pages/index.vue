b39d2e50e4e4b1bf72379eda11dfae85e540e2fb
Jesús Verduzco <jesus@verduzco.me>
2020-10-06 21:29:51 -0500

    Change

--- pages/index.vue
+++ pages/index.vue
@@ -7,7 +7,7 @@
       </div>
       <v-card>
         <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
+          Bienvenido a nuestra primera aplicación en producción.
         </v-card-title>
         <v-card-text>
           <p>
